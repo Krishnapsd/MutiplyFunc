242920a9bb3cbc395b359ca52792f1b6379c7f07
krishnapsd.thapaliya@gmail.com <krishnapsd.thapaliya@gmail.com>
2019-08-13 17:18:06 -0500

    Commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number, Counter, term;
+            string tryMore = "Y";
+            Console.WriteLine();
+            while (tryMore.ToUpper() == "Y")
+            {
+                Console.Write("Enter the number for Mupltiply :");
+                Number = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter the terms :");
+            term = Convert.ToInt32(Console.ReadLine());
+
+                for (Counter = 1; Counter <= term; Counter++)
+                {
+                    Console.WriteLine(Number + " x " + Counter + " =" + Number * Counter);
+                }
+                Console.WriteLine();
+                Console.Write("Do you want to Continue (Y/N) :");
+                tryMore = Console.ReadLine();
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
